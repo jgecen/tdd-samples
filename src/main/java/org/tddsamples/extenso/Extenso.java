bb3dd74c142488d0a5af283d5364340cebea3223
Ivan de Aguirre <ivan.aguirre@dextra-sw.com>
2013-07-31 22:51:03 -0300

    Refactoring Extenso

--- src/main/java/org/tddsamples/extenso/Extenso.java
+++ src/main/java/org/tddsamples/extenso/Extenso.java
@@ -6,48 +6,49 @@ import java.util.Map;
 
 public class Extenso {
 
-	private static Map<BigDecimal, String> constantes;
+	private static Map<BigDecimal, String> constants;
 	static {
-		constantes = new HashMap<>();
-		constantes.put(BigDecimal.ZERO, "zero");
-		constantes.put(BigDecimal.ONE, "um");
-		constantes.put(new BigDecimal(2), "dois");
-		constantes.put(BigDecimal.TEN, "dez");
-		constantes.put(new BigDecimal(11), "onze");
-		constantes.put(new BigDecimal(20), "vinte");
-		constantes.put(new BigDecimal(30), "trinta");
-		constantes.put(new BigDecimal(100), "cem");
-		constantes.put(new BigDecimal(200), "duzentos");
-		constantes.put(new BigDecimal(1000), "mil");
+		constants = new HashMap<>();
+		constants.put(BigDecimal.ZERO, "zero");
+		constants.put(BigDecimal.ONE, "um");
+		constants.put(new BigDecimal(2), "dois");
+		constants.put(BigDecimal.TEN, "dez");
+		constants.put(new BigDecimal(11), "onze");
+		constants.put(new BigDecimal(20), "vinte");
+		constants.put(new BigDecimal(30), "trinta");
+		constants.put(new BigDecimal(100), "cem");
+		constants.put(new BigDecimal(200), "duzentos");
+		constants.put(new BigDecimal(1000), "mil");
 	}
 
 	public static String parse(BigDecimal n) {
-		return getStringDecimo(n, 0).replace("e cem e", "cento e");
+		return getStringFromTenth(n, 0).replace("e cem e", "cento e");
 	}
 
-	private static String getStringUnidade(BigDecimal n, BigDecimal decimo) {
-		BigDecimal unidade = n.subtract(decimo);
-		if (unidade.compareTo(BigDecimal.ZERO) == 0) {
+	private static String getStringUnity(BigDecimal n, BigDecimal tenth) {
+		BigDecimal unity = n.subtract(tenth);
+		if (unity.compareTo(BigDecimal.ZERO) == 0) {
 			return "";
 		} else {
-			return " e " + constantes.get(unidade);
+			return " e " + constants.get(unity);
 		}
 	}
 
-	private static String getStringDecimo(BigDecimal numero, int pow) {
-		String s = constantes.get(numero);
-		if (pow > 0 && numero.compareTo(new BigDecimal(100)) == 0) {
+	private static String getStringFromTenth(BigDecimal n, int pow) {
+		String s = constants.get(n);
+		if (pow > 0 && n.compareTo(new BigDecimal(100)) == 0) {
 			s = "cento";
 		}
 
 		if (s == null) {
-			BigDecimal decimo = getDecimo(numero, new BigDecimal(10).pow(pow + 1));
-			s = getStringDecimo(decimo, pow + 1) + getStringUnidade(numero, decimo);
+			BigDecimal tenth = getTenth(n, pow + 1);
+			s = getStringFromTenth(tenth, pow + 1) + getStringUnity(n, tenth);
 		}
 		return s;
 	}
 
-	private static BigDecimal getDecimo(BigDecimal n, BigDecimal factor) {
+	private static BigDecimal getTenth(BigDecimal n, int pow) {
+		BigDecimal factor = new BigDecimal(10).pow(pow);
 		BigDecimal dezena = n.divideToIntegralValue(factor).multiply(factor);
 		return dezena;
 	}
